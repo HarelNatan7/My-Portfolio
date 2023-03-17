83b92f765e61b296f7a59156ae581f535276b428
Harel Natan <harelnatan7@gmail.com>
2023-03-17 16:12:00 +0200

    changed linkedin link

--- src/components/header/HeaderSocial.jsx
+++ src/components/header/HeaderSocial.jsx
@@ -6,7 +6,7 @@ import { BsFacebook } from 'react-icons/bs'
 const HeaderSocial = () => {
     return (
         <div className='header__socials'>
-            <a href="https://www.linkedin.com/in/yosikari/" target="_blank"><BsLinkedin /></a>
+            <a href="https://www.linkedin.com/in/harelnatan/" target="_blank"><BsLinkedin /></a>
             <a href="https://github.com/yosikari" target="_blank"><FaGithub /></a>
             <a href="https://www.facebook.com/yossi.karasik/" target="_blank"><BsFacebook /></a>
         </div>
